2f7bb8c0eb723468f06003a6c24d11dafd1ef624
Sam Baldwin <mail@sambaldwin.info>
2019-04-14 11:15:01 +0100

    Remove projects

--- site/templates/posts.php
+++ site/templates/posts.php
@@ -4,11 +4,11 @@
 
 <?php if ($page->children()) : ?>
   <ul>
-    <?php foreach ($page->children() as $project) : ?>
+    <?php foreach ($page->children() as $post) : ?>
       <li>
-        <a href="<?= $project->url() ?>"><?= $project->title() ?></a>
-        <time datetime="<?= $project->datePosted()->toDate('d-m-Y') ?>">
-          <?= $project->datePosted()->toDate('j F Y') ?>
+        <a href="<?= $post->url() ?>"><?= $post->title() ?></a>
+        <time datetime="<?= $post->datePosted()->toDate('d-m-Y') ?>">
+          <?= $post->datePosted()->toDate('j F Y') ?>
         </time>
       </li>
     <?php endforeach ?>
