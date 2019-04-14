8d1df16f35fe1530f6839b87ccb7504a57f185f8
Sam Baldwin <mail@sambaldwin.info>
2019-04-14 15:44:59 +0100

    Style post template

--- site/templates/post.php
+++ site/templates/post.php
@@ -1,19 +1,34 @@
 <?php snippet('header') ?>
 
-<h1><?php echo $page->title()->html() ?></h1>
-<time datetime="<?= $page->datePosted()->toDate('d-m-Y') ?>">
-  <?= $page->datePosted()->toDate('j F Y') ?>
-</time>
-
-<?= $page->text()->kirbytext() ?>
-
-<?php foreach ($page->images() as $image) : ?>
-  <figure>
-    <?= $image ?>
-    <?php if ($image->caption()->isNotEmpty()) : ?>
-      <figcaption><?= $image->caption()->kirbytextinline() ?></figcaption>
-    <?php endif; ?>
-  </figure>
-<?php endforeach ?>
+<article class="page">
+  <header class="page__header">
+    <h1 class="page__title"><?php echo $page->title()->html() ?></h1>
+    <button class="unset">Share</button>
+  </header>
+  <div class="page__main">
+    <?php if ($page->images()->isNotEmpty()) : ?>
+      <div class="page__images">
+      <?php foreach ($page->images() as $image) : ?>
+        <figure>
+          <?= $image ?>
+          <?php if ($image->caption()->isNotEmpty()) : ?>
+            <figcaption><?= $image->caption()->kirbytextinline() ?></figcaption>
+          <?php endif; ?>
+        </figure>
+      <?php endforeach ?>
+      </div>
+    <?php endif ?>
+    <?php if ($page->images()->isNotEmpty()) : ?>
+      <div class="page__text">
+        <?= $page->text()->kirbytext() ?>
+      </div>
+    <?php endif ?>
+  </div>
+  <footer class="page__footer">
+    <time datetime="<?= $page->datePosted()->toDate('d-m-Y') ?>">
+      Posted <?= $page->datePosted()->toDate('j F Y') ?>
+    </time>
+  </footer>
+</article>
 
 <?php snippet('footer') ?>
