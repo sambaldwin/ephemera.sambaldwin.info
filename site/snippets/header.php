918258f2e134fcb5e923fdec127fdfa013f16260
Sam Baldwin <mail@sambaldwin.info>
2019-04-14 11:43:23 +0100

    Add some basic styles

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -5,19 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" cntent="ie=edge">
     <title><?= e(!$page->isHomePage(), $page->title()->html() . ' – ') ?><?= $site->title() ?></title>
+    <?= css('assets/css/styles.css') ?>
   </head>
   <body>
 
-    <header role="banner">
+    <header class="site-header" role="banner">
       <a href="<?= $site->url() ?>"><?= $site->title() ?></a>
 
       <nav role="navigation">
-        <ul>
-          <?php foreach ($site->children()->listed() as $item): ?>
-            <li>
-              <a href="<?= $item->url() ?>"><?= $item->title()->html() ?></a>
-            </li>
-          <?php endforeach ?>
-        </ul>
+        <?php foreach ($site->children()->listed() as $item): ?>
+          <a href="<?= $item->url() ?>"><?= $item->title()->html() ?></a>
+        <?php endforeach ?>
       </nav>
     </header>
--- site/snippets/posts.php
+++ site/snippets/posts.php
@@ -1,12 +1,18 @@
 <?php if ($p->children()) : ?>
-  <ul>
+  <div class="posts">
     <?php foreach ($p->children() as $post) : ?>
-      <li>
-        <a href="<?= $post->url() ?>"><?= $post->title() ?></a>
-        <time datetime="<?= $post->datePosted()->toDate('d-m-Y') ?>">
-          <?= $post->datePosted()->toDate('j F Y') ?>
-        </time>
-      </li>
+      <article class="post__post post">
+        <a class="post__link" href="<?= $post->url() ?>">
+          <?php foreach ($post->images() as $image) : ?>
+            <figure>
+              <?= $image ?>
+              <?php if ($image->caption()->isNotEmpty()) : ?>
+                <figcaption><?= $image->caption()->kirbytextinline() ?></figcaption>
+              <?php endif; ?>
+            </figure>
+          <?php endforeach ?>
+        </a>
+      </article>
     <?php endforeach ?>
   </ul>
 <?php endif ?>
