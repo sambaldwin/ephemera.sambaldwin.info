133881cf530c9b6b36f34df102b1115cd6fd5890
Sam Baldwin <mail@sambaldwin.info>
2019-04-15 09:05:55 +0100

    Flip sort order in posts snippet

--- site/snippets/posts.php
+++ site/snippets/posts.php
@@ -1,6 +1,6 @@
 <?php if ($p->children()) : ?>
   <div class="posts">
-    <?php foreach ($p->children() as $post) : ?>
+    <?php foreach ($p->children()->flip() as $post) : ?>
       <article class="post__post post">
         <a class="post__link" href="<?= $post->url() ?>">
           <?php foreach ($post->images() as $image) : ?>
